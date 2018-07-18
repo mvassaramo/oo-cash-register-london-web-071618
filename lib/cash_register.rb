5826ee2b09d618a345aa6dd4895f435563137c5f
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-07-18 19:23:17 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
   
   def add_item(title, price, quantity=1)
     @quantity=quantity
-    @total = @total + price * quantity
+    @total = @total + (price * quantity)
   end 
   
   
