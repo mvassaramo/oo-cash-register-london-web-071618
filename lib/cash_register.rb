2380e3a42c368f7ef45207ef19d139db8f825a04
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-07-18 19:46:20 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,6 +16,8 @@ class CashRegister
   
   def apply_discount 
     @total = (1 - (@discount.to_f/100)) * @total
+    if @discount = 0 
+      return 
     return "After the discount, the total comes to $#{@total.to_i}."
    
   end
